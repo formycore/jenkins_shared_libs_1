f7a3f7c9e2521aeb54e204f3808e23a6bc1e7466
Sandeep <formycore@gmail.com>
2023-04-15 22:49:43 +0530

    docker image build added correct docker image build command

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call (String project, String hubUser, String ImageTag){
     sh """
-        docker build -t ${hubUser}/${project} .
+        docker image build -t ${hubUser}/${project} .
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
         dockere image tag ${hubUser}/${project} ${hubUser}/${project}:latest
 
