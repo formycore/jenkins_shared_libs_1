e88627a9ce517a8416e607fdf6598d3fc18aace9
Sandeep <formycore@gmail.com>
2023-04-20 19:21:08 +0530

    checking the comments

--- vars/ecrDockerBuild.groovy
+++ vars/ecrDockerBuild.groovy
@@ -1,6 +1,7 @@
 def call (String aws_account_id, String region, String ecr_repoName ){
+    // to build and tag the ecr image we need below two commands
     sh """
-        // to build and tag the ecr image we need below two commands
+        #To build and tag the ecr image we need below two commands
            docker build -t ${ecr_repoName} .
           docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
 
